c7ee146cf0c99cb5e1ae46689e80ed8e16547c27
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2022-05-15 18:06:03 -0400

    loading 4 items

---
+++ empower/src/main/java/empower/ca/util/ViewBindings.kt
@@ -0,0 +1,120 @@
+package empower.ca.util
+
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import empower.ca.R
+import empower.ca.databinding.ItemAdsBinding
+import empower.ca.databinding.ItemBannerBinding
+import empower.ca.databinding.ItemBasicBinding
+import empower.ca.databinding.ItemExposeBinding
+import empower.ca.model.Content
+
+
+fun loadData(view: ItemBasicBinding, content: Content, itemView: View) {
+    view.cardBasic.visibility = View.VISIBLE
+    view.header.text = content.header
+    view.title.text = content.title
+    view.description.text = content.description
+    view.buttonOperator.visibility = View.GONE
+    view.linkOperator.visibility = View.GONE
+
+    for (op in content.operators) {
+        if (op.type == "button") {
+            view.buttonOperator.text = op.text
+            view.buttonOperator.visibility = View.VISIBLE
+        }
+        if (op.type == "link") {
+            view.linkOperator.text = op.text
+            view.linkOperator.visibility = View.VISIBLE
+        }
+    }
+
+    Glide.with(itemView)
+        .load(content.image)
+        .centerCrop()
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .error(R.drawable.marvel)
+        .into(view.image)
+}
+
+fun loadData(view: ItemBannerBinding, content: Content, itemView: View) {
+    view.cardBanner.visibility = View.VISIBLE
+    view.header.text = content.header
+    view.title.text = content.title
+    view.description.text = content.description
+    view.buttonOperator.visibility = View.GONE
+    view.linkOperator.visibility = View.GONE
+
+    for (op in content.operators) {
+        if (op.type == "button") {
+            view.buttonOperator.text = op.text
+            view.buttonOperator.visibility = View.VISIBLE
+        }
+        if (op.type == "link") {
+            view.linkOperator.text = op.text
+            view.linkOperator.visibility = View.VISIBLE
+        }
+    }
+
+    Glide.with(itemView)
+        .load(content.image)
+        .centerCrop()
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .error(R.drawable.marvel)
+        .into(view.image)
+}
+
+fun loadData(view: ItemExposeBinding, content: Content, itemView: View) {
+    view.cardExpose.visibility = View.VISIBLE
+    view.header.text = content.header
+    view.title.text = content.title
+    view.description.text = content.description
+    view.buttonOperator.visibility = View.GONE
+    view.linkOperator.visibility = View.GONE
+
+    for (op in content.operators) {
+        if (op.type == "button") {
+            view.buttonOperator.text = op.text
+            view.buttonOperator.visibility = View.VISIBLE
+        }
+        if (op.type == "link") {
+            view.linkOperator.text = op.text
+            view.linkOperator.visibility = View.VISIBLE
+        }
+    }
+
+    Glide.with(itemView)
+        .load(content.image)
+        .centerCrop()
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .error(R.drawable.marvel)
+        .into(view.image)
+}
+
+fun loadData(view: ItemAdsBinding, content: Content, itemView: View) {
+    view.cardAds.visibility = View.VISIBLE
+    view.header.text = content.header
+    view.title.text = content.title
+    view.description.text = content.description
+    view.buttonOperator.visibility = View.GONE
+    view.linkOperator.visibility = View.GONE
+
+    for (op in content.operators) {
+        if (op.type == "button") {
+            view.buttonOperator.text = op.text
+            view.buttonOperator.visibility = View.VISIBLE
+        }
+        if (op.type == "link") {
+            view.linkOperator.text = op.text
+            view.linkOperator.visibility = View.VISIBLE
+        }
+    }
+
+    Glide.with(itemView)
+        .load(content.image)
+        .centerCrop()
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .error(R.drawable.marvel)
+        .into(view.image)
+}
