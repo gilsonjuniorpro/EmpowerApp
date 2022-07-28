7e72dceceefdf1812eefeb17a7d51a1f1ec6cc22
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2022-07-28 07:02:04 -0400

    ADD new API endpoint

--- empower/src/main/java/empower/ca/core/Empower.kt
+++ empower/src/main/java/empower/ca/core/Empower.kt
@@ -8,10 +8,10 @@ import empower.ca.fragment.EmpowerFragment.Companion.EMPOWER_CONTENT_OBJECT
 import empower.ca.fragment.EmpowerFragment.Companion.EMPOWER_OPTION_OBJECT
 import empower.ca.fragment.EmpowerFragment.Companion.EMPOWER_POWER_OBJECT
 import empower.ca.fragment.EmpowerFragment.Companion.INSTANCE_HASH_CODE
-import empower.ca.model.Content
 import empower.ca.model.ContentWrapper
 import empower.ca.sealed.Option
 import empower.ca.sealed.Power
+import empower.ca.util.getPower
 
 object Empower {
 
@@ -19,6 +19,20 @@ object Empower {
         val bundle = Bundle()
         bundle.putParcelable(EMPOWER_POWER_OBJECT, power)
         bundle.putParcelable(EMPOWER_OPTION_OBJECT, option)
+        bundle.putParcelable(EMPOWER_CONTENT_OBJECT, contentWrapper)
+
+        val empowerInstance = EmpowerFragment.newInstance()
+        val empowerHashCode = empowerInstance.hashCode()
+        bundle.putInt(INSTANCE_HASH_CODE, empowerHashCode)
+        empowerInstance.arguments = bundle
+
+        return empowerInstance
+    }
+
+    fun fragment(contentWrapper: ContentWrapper): Fragment {
+        val bundle = Bundle()
+        bundle.putParcelable(EMPOWER_POWER_OBJECT, getPower(contentWrapper.contentType))
+        bundle.putParcelable(EMPOWER_OPTION_OBJECT, Option.Container(contentWrapper.containerTitle))
         bundle.putParcelable(EMPOWER_CONTENT_OBJECT, contentWrapper)
 
         val empowerInstance = EmpowerFragment.newInstance()
